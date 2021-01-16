9402800902cc12af89aa840130b4a14c46c417bd
Kwee H Tan <tan.k.h@juno.com>
2021-01-16 14:52:03 -0800

    Put database in d:/database/nd

--- ch05/ch05-run-nerd-dinner_part-1.ps1
+++ ch05/ch05-run-nerd-dinner_part-1.ps1
@@ -4,7 +4,7 @@ docker container run -d `
 
 docker container run -d -p 1433:1433 `
  --name nerd-dinner-db `
- -v C:\databases\nd:C:\data `
+ -v D:\databases\nd:C:\data `
  dockeronwindows/ch03-nerd-dinner-db:2e;
 
 docker container run -d `
