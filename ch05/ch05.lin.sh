b1cce4423c983da98c611e45d6b5c7a9074f7523
Kwee H Tan <tan.k.h@juno.com>
2021-02-02 20:43:26 -0800

    Trying to coordinate between Traefik on linux and the rest on Windows

---
+++ ch05/ch05.lin.sh
@@ -0,0 +1,13 @@
+# Windows traefik version does not run:
+# full invocation string: docker container run -d -p 80:80 -p 8080:8080 --name reverse-proxy -v \\.\pipe\docker_engine:\\.\pipe\docker_engine sixeyed/traefik:v1.7.8-windowsservercore-ltsc2019 --api --docker --docker.endpoint=npipe:////./pipe/docker_engine --logLevel=DEBUG
+# arguments to docker: -d -p 80:80 -p 8080:8080 --name reverse-proxy -v \\.\pipe\docker_engine:\\.\pipe\docker_engine
+# image to run: sixeyed/traefik:v1.7.8-windowsservercore-ltsc2019
+# arguments to traefik: --api --docker --docker.endpoint=npipe:////./pipe/docker_engine --logLevel=DEBUG
+# Linux:
+# docker container run -d -p 80:80 -p 8080:8080 --name reverse-proxy -v /var/run/docker.sock:/var/run/docker.sock traefik:v1.7.8 --api --docker ???--docker.endpoint=npipe:////./pipe/docker_engine --logLevel=DEBUG
+# --network nd-net
+# --api # enable api/dashboard
+# ---docker?
+# --docker.endpoint?
+
+docker container run -d -p 80:80 --name reverse-proxy -v /var/run/docker.sock:/var/run/docker.sock traefik:v1.7.8 --api --logLevel=DEBUG
