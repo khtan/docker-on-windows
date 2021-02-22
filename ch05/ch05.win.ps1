9fbdfe837ab95f98fdffa28de20ee17bcf9544f0
Kwee H Tan <tan.k.h@juno.com>
2021-02-21 16:56:40 -0800

    update

--- ch05/ch05.win.ps1
+++ ch05/ch05.win.ps1
@@ -15,7 +15,7 @@ docker container run -d `
 # full invocation string: docker container run -d -p 80:80 -p 8080:8080 --name reverse-proxy -v \\.\pipe\docker_engine:\\.\pipe\docker_engine sixeyed/traefik:v1.7.8-windowsservercore-ltsc2019 --api --docker --docker.endpoint=npipe:////./pipe/docker_engine --logLevel=DEBUG
 # arguments to docker: -d -p 80:80 -p 8080:8080 --name reverse-proxy -v \\.\pipe\docker_engine:\\.\pipe\docker_engine
 # image to run: sixeyed/traefik:v1.7.8-windowsservercore-ltsc2019
-# arguments to traefik: --api --docker --docker.endpoint=npipe:////./pipe/docker_engine --logLevel=DEBUG
+# arguments to traefik: traefik178 --api --api.dashboard --docker --docker.endpoint=npipe:////./pipe/docker_engine --logLevel=DEBUG
 docker container run -d `
   --name nerd-dinner-homepage `
   -l "traefik.frontend.rule=Host:nerddinner.local;Path:/,/css/site.css" `
