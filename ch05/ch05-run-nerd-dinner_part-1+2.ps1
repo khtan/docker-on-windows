85cf5a089f95c3f2aea060c1f627fa601bfc39f6
Kwee H Tan <tan.k.h@juno.com>
2021-02-23 20:58:09 -0800

    part1+2 updated and working on //air

--- ch05/ch05-run-nerd-dinner_part-1+2.ps1
+++ ch05/ch05-run-nerd-dinner_part-1+2.ps1
@@ -11,8 +11,8 @@ docker container run -d `
   --name nerd-dinner-save-handler `
   dockeronwindows/ch05-nerd-dinner-save-handler:2e;
 
-docker container run -d -p 80:80 -p 8080:8080 `
- --name reverse-proxy ` --isolation process
+docker container run -d -p 80 -p 8080 `
+  --name reverse-proxy `
  -v \\.\pipe\docker_engine:\\.\pipe\docker_engine `
  tankh1/traefik:v1.7.8-win2004 `
  --api --docker --docker.endpoint=npipe:////./pipe/docker_engine --logLevel=DEBUG
