739856a398dd59764f5c40d5003d00b99e63b1c3
Kwee H Tan <tan.k.h@juno.com>
2021-02-26 14:03:26 -0800

    air: ps1+2 reverse-proxy working

--- ch05/ch05-run-nerd-dinner_part-1+2.ps1
+++ ch05/ch05-run-nerd-dinner_part-1+2.ps1
@@ -11,7 +11,7 @@ docker container run -d `
   --name nerd-dinner-save-handler `
   dockeronwindows/ch05-nerd-dinner-save-handler:2e;
 
-docker container run -d -p 80 -p 8080 `
+docker container run -d -p 80 -p 8080 --isolation process `
   --name reverse-proxy `
  -v \\.\pipe\docker_engine:\\.\pipe\docker_engine `
  tankh1/traefik:v1.7.8-win2004 `
